dd366956be95f3f0b430fb94bcb919fbdea6ef57
Stefanie Bosch <stefaniebosch333@gmail.com>
2019-11-04 18:31:35 +0100

    Update parking_maneuver.py
    completed assignment 3-1

--- src/AutoMiny-exercises/simple_parking_maneuver/src/parking_maneuver.py
+++ src/AutoMiny-exercises/simple_parking_maneuver/src/parking_maneuver.py
@@ -26,18 +26,18 @@ def callbackBackwardLongitudinal(request):
         return ParkingManeuverResponse(
             "ERROR: Request can only be 'left' or 'right'")
 
-    driving_direction_pub1.publish(0.30)
+    driving_direction_pub1.publish(0.27)
 
-#     rospy.sleep(10)
-#     pub_back.publish(0.10)
+    rospy.sleep(5)
+    driving_direction_pub2.publish(0.22)
+    
+    #rospy.sleep(5)
+    #pub_back.publish(0.10)
 
-    rospy.sleep(10)
-    driving_direction_pub2.publish(0.30)
+    rospy.sleep(5)
+    pub_forward.publish(0.22)
 
-    rospy.sleep(10)
-    pub_forward.publish(0.1)
-
-    rospy.sleep(10)
+    rospy.sleep(5)
     return ParkingManeuverResponse("FINISHED")
 
 
